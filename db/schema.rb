8c7f47c2c02a1be364b1858178363f4136d9c174
Kevin Chang <changkaimin.big@gmail.com>
2014-10-07 11:31:18 +0800

    register_new_user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140917171044) do
+ActiveRecord::Schema.define(version: 20141006131434) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 20140917171044) do
     t.datetime "updated_at"
   end
 
+  create_table "post_categories", force: true do |t|
+    t.integer "category_id"
+    t.integer "post_id"
+  end
+
   create_table "posts", force: true do |t|
     t.string   "url"
     t.string   "title"
@@ -40,6 +45,7 @@ ActiveRecord::Schema.define(version: 20140917171044) do
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
